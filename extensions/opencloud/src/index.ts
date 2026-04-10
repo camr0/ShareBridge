465c707d09435b000ef0741bdede0304dcdb6e6b
Ali Aslam <ali.aslam@zohomail.com>
2026-04-09 22:48:27 -0400

    fix: import vue3-gettext in index.ts so AMD loader provides host's initialized instance

--- extensions/opencloud/src/index.ts
+++ extensions/opencloud/src/index.ts
@@ -1,4 +1,5 @@
 import { computed } from 'vue'
+import 'vue3-gettext' // ensure AMD loader provides host's initialized gettext instance
 import { defineWebApplication } from '@opencloud-eu/web-pkg'
 import { useShareBridgeExtension } from './composables/useShareBridgeExtension'
 
